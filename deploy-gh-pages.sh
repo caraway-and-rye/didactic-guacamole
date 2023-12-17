5841fd457258d947175491e8bb469e71bee4abdf
caraway-and-rye <anca.lumei@gmail.com>
2023-12-17 23:01:46 +0000

    Maybe this works

---
+++ deploy-gh-pages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+git checkout --orphan gh-pages
+npm run build
+git --work-tree dist add --all
+git --work-tree dist commit -m 'Deploy'
+git push origin HEAD:gh-pages --force
+rm -r dist
+git checkout -f main
+git branch -D gh-pages
