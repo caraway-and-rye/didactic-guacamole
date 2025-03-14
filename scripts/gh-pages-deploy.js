cfcc9ec0e6cd3dc801e97088521d3ffd0899e22b
caraway-and-rye <anca.lumei@gmail.com>
2025-03-14 09:43:08 +0000

    Add also the deploy script

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const execa = require("execa");
+const fs = require("fs");
+
+(async () => {
+try {
+    await execa("git", ["update-index", "--refresh"]); 
+    const { stdout } = await execa("git", ["diff-index", "HEAD"]);
+    if (stdout) {
+        console.log("Please stash or commit changes first!");
+        process.exit(1);
+    }
+
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    console.log("Building...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", folderName]);
+    console.log("Successfully deployed");
+} catch (e) {
+    console.log(e.message);
+    process.exit(1);
+}
+finally {
+    await execa("git", ["checkout", "-f", "master"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+}
+})();
